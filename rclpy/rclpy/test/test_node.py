1c8909c9709a0cbaed7f3084557ee4c3fb1ff380
hfz <you@example.com>
2020-10-09 18:24:43 -0700

    first commit

---
+++ rclpy/rclpy/test/test_node.py
@@ -0,0 +1,155 @@
+# Copyright 2017 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from rcl_interfaces.srv import GetParameters
+import rclpy
+from rclpy.exceptions import InvalidServiceNameException
+from rclpy.exceptions import InvalidTopicNameException
+from test_msgs.msg import Primitives
+
+TEST_NODE = 'my_node'
+TEST_NAMESPACE = '/my_ns'
+
+
+class TestNode(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+        cls.node = rclpy.create_node(TEST_NODE, namespace=TEST_NAMESPACE)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.node.destroy_node()
+        rclpy.shutdown()
+
+    def test_accessors(self):
+        self.assertIsNotNone(self.node.handle)
+        with self.assertRaises(AttributeError):
+            self.node.handle = 'garbage'
+        self.assertEqual(self.node.get_name(), TEST_NODE)
+        self.assertEqual(self.node.get_namespace(), TEST_NAMESPACE)
+
+    def test_create_publisher(self):
+        self.node.create_publisher(Primitives, 'chatter')
+        with self.assertRaisesRegex(InvalidTopicNameException, 'must not contain characters'):
+            self.node.create_publisher(Primitives, 'chatter?')
+        with self.assertRaisesRegex(InvalidTopicNameException, 'must not start with a number'):
+            self.node.create_publisher(Primitives, '/chatter/42_is_the_answer')
+        with self.assertRaisesRegex(ValueError, 'unknown substitution'):
+            self.node.create_publisher(Primitives, 'chatter/{bad_sub}')
+
+    def test_create_subscription(self):
+        self.node.create_subscription(Primitives, 'chatter', lambda msg: print(msg))
+        with self.assertRaisesRegex(InvalidTopicNameException, 'must not contain characters'):
+            self.node.create_subscription(Primitives, 'chatter?', lambda msg: print(msg))
+        with self.assertRaisesRegex(InvalidTopicNameException, 'must not start with a number'):
+            self.node.create_subscription(Primitives, '/chatter/42ish', lambda msg: print(msg))
+        with self.assertRaisesRegex(ValueError, 'unknown substitution'):
+            self.node.create_subscription(Primitives, 'foo/{bad_sub}', lambda msg: print(msg))
+
+    def test_create_client(self):
+        self.node.create_client(GetParameters, 'get/parameters')
+        with self.assertRaisesRegex(InvalidServiceNameException, 'must not contain characters'):
+            self.node.create_client(GetParameters, 'get/parameters?')
+        with self.assertRaisesRegex(InvalidServiceNameException, 'must not start with a number'):
+            self.node.create_client(GetParameters, '/get/42parameters')
+        with self.assertRaisesRegex(ValueError, 'unknown substitution'):
+            self.node.create_client(GetParameters, 'foo/{bad_sub}')
+
+    def test_create_service(self):
+        self.node.create_service(GetParameters, 'get/parameters', lambda req: None)
+        with self.assertRaisesRegex(InvalidServiceNameException, 'must not contain characters'):
+            self.node.create_service(GetParameters, 'get/parameters?', lambda req: None)
+        with self.assertRaisesRegex(InvalidServiceNameException, 'must not start with a number'):
+            self.node.create_service(GetParameters, '/get/42parameters', lambda req: None)
+        with self.assertRaisesRegex(ValueError, 'unknown substitution'):
+            self.node.create_service(GetParameters, 'foo/{bad_sub}', lambda req: None)
+
+    def test_service_names_and_types(self):
+        # test that it doesn't raise
+        self.node.get_service_names_and_types()
+
+    def test_topic_names_and_types(self):
+        # test that it doesn't raise
+        self.node.get_topic_names_and_types(no_demangle=True)
+        self.node.get_topic_names_and_types(no_demangle=False)
+
+    def test_node_names(self):
+        # test that it doesn't raise
+        self.node.get_node_names()
+
+    def test_count_publishers_subscribers(self):
+        short_topic_name = 'chatter'
+        fq_topic_name = '%s/%s' % (TEST_NAMESPACE, short_topic_name)
+
+        self.assertEqual(0, self.node.count_publishers(fq_topic_name))
+        self.assertEqual(0, self.node.count_subscribers(fq_topic_name))
+
+        self.node.create_publisher(Primitives, short_topic_name)
+        self.assertEqual(1, self.node.count_publishers(short_topic_name))
+        self.assertEqual(1, self.node.count_publishers(fq_topic_name))
+
+        self.node.create_subscription(Primitives, short_topic_name, lambda msg: print(msg))
+        self.assertEqual(1, self.node.count_subscribers(short_topic_name))
+        self.assertEqual(1, self.node.count_subscribers(fq_topic_name))
+
+        self.node.create_subscription(Primitives, short_topic_name, lambda msg: print(msg))
+        self.assertEqual(2, self.node.count_subscribers(short_topic_name))
+        self.assertEqual(2, self.node.count_subscribers(fq_topic_name))
+
+        # error cases
+        with self.assertRaisesRegex(TypeError, 'Argument topic_name is not a'):
+            self.node.count_subscribers(1)
+        with self.assertRaisesRegex(ValueError, 'is invalid'):
+            self.node.count_subscribers('42')
+        with self.assertRaisesRegex(ValueError, 'is invalid'):
+            self.node.count_publishers('42')
+
+    def test_node_logger(self):
+        node_logger = self.node.get_logger()
+        expected_name = '%s.%s' % (TEST_NAMESPACE.replace('/', '.')[1:], TEST_NODE)
+        self.assertEqual(node_logger.name, expected_name)
+        node_logger.set_level(rclpy.logging.LoggingSeverity.INFO)
+        node_logger.debug('test')
+
+
+class TestCreateNode(unittest.TestCase):
+
+    def test_use_global_arguments(self):
+        rclpy.init(args=['process_name', '__node:=global_node_name'])
+        try:
+            node1 = rclpy.create_node('my_node', namespace='/my_ns', use_global_arguments=True)
+            node2 = rclpy.create_node('my_node', namespace='/my_ns', use_global_arguments=False)
+            self.assertEqual('global_node_name', node1.get_name())
+            self.assertEqual('my_node', node2.get_name())
+            node1.destroy_node()
+            node2.destroy_node()
+        finally:
+            rclpy.shutdown()
+
+    def test_node_arguments(self):
+        rclpy.init()
+        try:
+            node = rclpy.create_node('my_node', namespace='/my_ns', cli_args=['__ns:=/foo/bar'])
+            self.assertEqual('/foo/bar', node.get_namespace())
+            node.destroy_node()
+        finally:
+            rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ rcutils/src/filesystem.c
@@ -0,0 +1,166 @@
+// Copyright 2017 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "rcutils/filesystem.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <direct.h>
+#endif  // _WIN32
+
+#include "rcutils/format_string.h"
+
+bool
+rcutils_get_cwd(char * buffer, size_t max_length)
+{
+  if (NULL == buffer) {
+    return false;
+  }
+#ifdef _WIN32
+  if (NULL == _getcwd(buffer, (int)max_length)) {
+    return false;
+  }
+#else
+  if (NULL == getcwd(buffer, max_length)) {
+    return false;
+  }
+#endif  // _WIN32
+  return true;
+}
+
+bool
+rcutils_is_directory(const char * abs_path)
+{
+  struct stat buf;
+  if (stat(abs_path, &buf) < 0) {
+    return false;
+  }
+#ifdef _WIN32
+  return (buf.st_mode & S_IFDIR) == S_IFDIR;
+#else
+  return S_ISDIR(buf.st_mode);
+#endif  // _WIN32
+}
+
+bool
+rcutils_is_file(const char * abs_path)
+{
+  struct stat buf;
+  if (stat(abs_path, &buf) < 0) {
+    return false;
+  }
+#ifdef _WIN32
+  return (buf.st_mode & S_IFREG) == S_IFREG;
+#else
+  return S_ISREG(buf.st_mode);
+#endif  // _WIN32
+}
+
+bool
+rcutils_exists(const char * abs_path)
+{
+  struct stat buf;
+  if (stat(abs_path, &buf) < 0) {
+    return false;
+  }
+  return true;
+}
+
+bool
+rcutils_is_readable(const char * abs_path)
+{
+  struct stat buf;
+  if (stat(abs_path, &buf) < 0) {
+    return false;
+  }
+#ifdef _WIN32
+  if (!(buf.st_mode & _S_IREAD)) {
+#else
+  if (!(buf.st_mode & S_IRUSR)) {
+#endif  // _WIN32
+    return false;
+  }
+  return true;
+}
+
+bool
+rcutils_is_writable(const char * abs_path)
+{
+  struct stat buf;
+  if (stat(abs_path, &buf) < 0) {
+    return false;
+  }
+#ifdef _WIN32
+  if (!(buf.st_mode & _S_IWRITE)) {
+#else
+  if (!(buf.st_mode & S_IWUSR)) {
+#endif  // _WIN32
+    return false;
+  }
+  return true;
+}
+
+bool
+rcutils_is_readable_and_writable(const char * abs_path)
+{
+  struct stat buf;
+  if (stat(abs_path, &buf) < 0) {
+    return false;
+  }
+#ifdef _WIN32
+  // NOTE(marguedas) on windows all writable files are readable
+  // hence the following check is equivalent to "& _S_IWRITE"
+  if (!((buf.st_mode & _S_IWRITE) && (buf.st_mode & _S_IREAD))) {
+#else
+  if (!((buf.st_mode & S_IWUSR) && (buf.st_mode & S_IRUSR))) {
+#endif  // _WIN32
+    return false;
+  }
+  return true;
+}
+
+char *
+rcutils_join_path(
+  const char * left_hand_path,
+  const char * right_hand_path,
+  rcutils_allocator_t allocator)
+{
+  if (NULL == left_hand_path) {
+    return NULL;
+  }
+  if (NULL == right_hand_path) {
+    return NULL;
+  }
+
+#ifdef  _WIN32
+  const char * delimiter = "\\";
+#else
+  const char * delimiter = "/";
+#endif  // _WIN32
+
+  return rcutils_format_string(allocator, "%s%s%s", left_hand_path, delimiter, right_hand_path);
+}
+
+#ifdef __cplusplus
+}
+#endif
